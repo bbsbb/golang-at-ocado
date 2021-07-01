9e4c82f314bbb78b3fb03eefc553a72d731c69b0
BB <boyan@typed.wtf>
2021-07-01 19:45:50 +0300

    Yoloing like a yoloer.

--- week-03/fulfillment-solution/sort/fulfillment-service/service.go
+++ week-03/fulfillment-solution/sort/fulfillment-service/service.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"sync"
 
 	"github.com/bbsbb/go-at-ocado/sort/gen"
 	"github.com/preslavmihaylov/ordertocubby"
@@ -14,41 +15,58 @@ const cubbiesCnt = 10
 func newFulfillmentService(client gen.SortingRobotClient) gen.FulfillmentServer {
 	fs := &fulfillmentService{
 		sortingRobot: client,
+		orders:       make(map[string]*gen.FullfillmentStatus),
 	}
 	fs.ordersCh = scheduleWork(fs.processBatch)
 	return fs
 }
 
 type fulfillmentService struct {
-	sortingRobot gen.SortingRobotClient
-	ordersCh     chan []*gen.Order
+	sortingRobot      gen.SortingRobotClient
+	ordersCh          chan []*gen.Order
+	orders            map[string]*gen.FullfillmentStatus
+	totallyConcurrent sync.Mutex
 }
 
 func (fs *fulfillmentService) processBatch(orders []*gen.Order) {
-	ordersToCubbies := mapOrdersToCubbies(orders)
 
-	for _, order := range orders {
-		for _, item := range order.Items {
-			_ = item
-
-			resp, err := fs.sortingRobot.PickItem(context.Background(), &gen.Empty{})
-			if err != nil {
-				// What do?
-			}
-
-			cubbyID := getCubbyForItem(resp.Item, ordersToCubbies, orders)
-			_, err = fs.sortingRobot.PlaceInCubby(context.Background(), &gen.PlaceInCubbyRequest{
-				Cubby: &gen.Cubby{Id: cubbyID},
-			})
-			if err != nil {
-				// What do?
-			}
-		}
+	for k := range fs.orders {
+		log.Println(k)
 	}
+
+	// ordersToCubbies := mapOrdersToCubbies(orders)
+
+	// for _, order := range orders {
+	// 	for _, item := range order.Items {
+	// 		_ = item
+
+	// 		resp, err := fs.sortingRobot.PickItem(context.Background(), &gen.Empty{})
+	// 		if err != nil {
+	// 			// What do?
+	// 		}
+
+	// 		cubbyID := getCubbyForItem(resp.Item, orders, ordersToCubbies)
+	// 		_, err = fs.sortingRobot.PlaceInCubby(context.Background(), &gen.PlaceInCubbyRequest{
+	// 			Cubby: &gen.Cubby{Id: cubbyID},
+	// 		})
+	// 		if err != nil {
+	// 			// What do?
+	// 		}
+	// 	}
+	// }
 }
 
 func (fs *fulfillmentService) LoadOrders(ctx context.Context, in *gen.LoadOrdersRequest) (*gen.CompleteResponse, error) {
 	go func() {
+		fs.totallyConcurrent.Lock()
+		for _, o := range in.Orders {
+			fs.orders[o.Id] = &gen.FullfillmentStatus{
+				Order: o,
+				Cubby: &gen.Cubby{},
+				State: gen.OrderState_PENDING,
+			}
+		}
+		fs.totallyConcurrent.Unlock()
 		fs.ordersCh <- in.Orders
 	}()
 
@@ -84,12 +102,16 @@ func mapOrderToCubby(usedCubbies map[string]bool, id string, cubbiesCnt int) str
 	}
 }
 
-func getOrderForItem(item *gen.Item, orders[]*gen.Order) *gen.Order{
+func getOrderForItem(item *gen.Item, orders []*gen.Order) *gen.Order {
 	var match *gen.Order
 	return match
 }
 
-func getCubbyForItem(item *gen.Item, , orders []*gen.Order, cubbyMapping map[string]string) string {
+func getCubbyForItem(item *gen.Item, orders []*gen.Order, cubbyMapping map[string]string) string {
+	// We get an item
+	// We solve it to order - handle item in two possible orders
+	// We lookup the cubbyId for the orders
+
 	log.Println(mapOrdersToCubbies)
 	log.Fatal("DONE")
 	return "1"
